3242d19b31bb528c188df91088230d3c83506fe3
Simon Thiel <simonthiel2@gmail.com>
2023-01-19 14:44:55 +0100

    currated into one list

---
+++ createList.py
@@ -0,0 +1,34 @@
+from os import walk
+import os.path
+
+hostfile = 'hosts'
+
+files = []
+for (dirpath, dirnames, filenames) in walk('data'):
+    for file in filenames:
+        path = os.path.join(dirpath, file)
+        files.append(path)
+    break
+
+number_of_entries = 0
+
+for file in files:
+    with open(file) as current_file:
+        for line in current_file:
+            if '#' in line:
+                continue
+            if line.strip():
+                number_of_entries += 1
+
+host = open(hostfile, 'w')
+host.write('# Title: Currated List by Cuupa\n')
+host.write('# Number of entries: ' + str(number_of_entries) + '\n')
+
+for file in files:
+    host.write('\n')
+    with open(file) as current_file:
+        for line in current_file:
+            print(line.rstrip())
+            host.write(line.rstrip())
+            host.write('\n')
+host.close()
